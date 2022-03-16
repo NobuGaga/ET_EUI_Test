d7a4577c146ab4fb916db3ba593e4b887b849455
NobuGaga <39802895+NobuGaga@users.noreply.github.com>
2022-03-16 20:45:19 +0800

    Update Fish World Battle Code

--- OtherProgram/FishWorld/Unity/Codes/ModelView/Demo/Battle/UnitComponent/BattleUnitViewComponent.cs
+++ OtherProgram/FishWorld/Unity/Codes/ModelView/Demo/Battle/UnitComponent/BattleUnitViewComponent.cs
@@ -4,7 +4,7 @@ namespace ET
 {
 	// 战斗单位组件, 用以拓展 Unit 单位, 作一些战斗用处理
 	// 用来储存战斗用视图相关数据的基础单位
-	public class BattleUnitViewComponent : Entity, IAwake, IDestroy
+	public class BattleUnitViewComponent : Entity, IAwake, IUpdate, IDestroy
 	{
 		public Transform NodeParent;
 	}
